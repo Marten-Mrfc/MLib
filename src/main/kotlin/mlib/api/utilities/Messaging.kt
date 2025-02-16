f700023943efd22afe8379f5b8360e7c5fb5abdf
Marten Mrfc <101009922+Marten-Mrfc@users.noreply.github.com>
2025-02-16 19:58:19 +0100

    [Library] Messaging try 3

--- src/main/kotlin/mlib/api/utilities/Messaging.kt
+++ src/main/kotlin/mlib/api/utilities/Messaging.kt
@@ -1,5 +1,7 @@
 package mlib.api.utilities
 
+import mlib.api.MLib
+import mlib.api.architecture.extensions.server
 import mlib.api.architecture.extensions.softDepend
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.minimessage.MiniMessage
@@ -7,45 +9,55 @@ import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 import org.bukkit.plugin.Plugin
 
-class Messaging(private val plugin: Plugin) : Initializable {
-    private val mm = MiniMessage.builder().build()
-    private val placeholderAPI by lazy { plugin.softDepend<Plugin>("PlaceholderAPI") }
+private val mm = MiniMessage.builder().build()
 
-    override fun initialize() {}
-
-    private fun String.withPlaceholders(player: Player?): String {
-        return if (player != null && placeholderAPI != null) {
-            me.clip.placeholderapi.PlaceholderAPI.setPlaceholders(player, this)
-        } else {
-            this
+private fun getPlugin(any: Any): Plugin? {
+    return when (any) {
+        is Plugin -> any
+        is CommandSender -> server.pluginManager.plugins.firstOrNull { plugin ->
+            MLib.instances.containsKey(plugin)
         }
+        else -> null
     }
+}
 
-    fun String.asMini(player: Player? = null): Component {
-        return mm.deserialize(withPlaceholders(player))
-    }
+private fun String.withPlaceholders(player: Player?): String {
+    val plugin = getPlugin(player ?: this) ?: return this
+    val placeholderAPI = plugin.softDepend<Plugin>("PlaceholderAPI")
 
-    fun Component.notMiniText(): String {
-        return mm.serialize(this).replace("\\<", "<")
+    return if (player != null && placeholderAPI != null) {
+        me.clip.placeholderapi.PlaceholderAPI.setPlaceholders(player, this)
+    } else {
+        this
     }
+}
 
-    fun Component.notMini(): String {
-        return mm.serialize(this)
-    }
+fun String.asMini(player: Player? = null): Component {
+    return mm.deserialize(withPlaceholders(player))
+}
 
-    fun CommandSender.sendMini(message: String, player: Player? = null) {
-        sendMessage(message.withPlaceholders(player).asMini())
-    }
+fun Component.notMiniText(): String {
+    return mm.serialize(this).replace("\\<", "<")
+}
 
-    fun CommandSender.error(message: String, player: Player? = null) {
-        sendMessage("<red><bold>Error</bold><gray> | <white> $message".withPlaceholders(player).asMini())
-    }
+fun Component.notMini(): String {
+    return mm.serialize(this)
+}
 
-    fun CommandSender.message(message: String, player: Player? = null) {
-        sendMessage("<gold><bold>${plugin.name}</bold><gray> | <white> $message".withPlaceholders(player).asMini())
-    }
+fun CommandSender.sendMini(message: String, player: Player? = null) {
+    sendMessage(message.withPlaceholders(player).asMini())
+}
 
-    fun message(message: String, player: Player? = null): Component {
-        return "<gold><bold>${plugin.name}</bold><gray> | <white> $message".withPlaceholders(player).asMini()
-    }
+fun CommandSender.error(message: String, player: Player? = null) {
+    sendMessage("<red><bold>Error</bold><gray> | <white> $message".withPlaceholders(player).asMini())
+}
+
+fun CommandSender.message(message: String, player: Player? = null) {
+    val plugin = getPlugin(this) ?: return
+    sendMessage("<gold><bold>${plugin.name}</bold><gray> | <white> $message".withPlaceholders(player).asMini())
+}
+
+fun String.asPluginMessage(player: Player? = null): Component {
+    val plugin = getPlugin(this) ?: return asMini(player)
+    return "<gold><bold>${plugin.name}</bold><gray> | <white> $this".withPlaceholders(player).asMini()
 }
